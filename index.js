0a6597ad523c4f3c16cff2c2a06e7a8db782ede3
Fernanda <engcivil.fernandasouza@gmail.com>
2021-12-03 18:13:02 -0300

    implantacao do render das tarefas

--- index.js
+++ index.js
@@ -1,35 +1,82 @@
+const $todo = document.querySelector('.todo');
+
+const $listTasks = document.createElement('ul');
+const $inputAddTask = document.createElement('input');
+const $inputCheckBox = document.createElement('input');
+$inputCheckBox.setAttribute('type', 'checkbox');
+const $buttonAddTask = document.createElement('button');
+$buttonAddTask.textContent = 'Add tarefa';
+
+$todo.appendChild($inputAddTask);
+$todo.appendChild($inputCheckBox);
+$todo.appendChild($buttonAddTask);
+$todo.appendChild($listTasks);
+
 let tasks = [];
-// {name: string, completed: boolean}
+
 const addTask = (name, status) => {
   tasks.push({ name: name, completed: status });
+  renderTasks(tasks);
 };
 
 const removeTask = (position) => {
   const tasksNew = [];
-  for (const index in tasks) {
-    if (position !== Number(index)) tasksNew.push(tasks[index]);
+
+  for (const i in tasks) {
+    if (i !== position) tasksNew.push(tasks[i]);
   }
+
   tasks = tasksNew;
+  renderTasks(tasks);
 };
 
 const toggleStatusTask = (position) => {
   const tasksNew = [];
-  for (const index in tasks) {
-    const name = tasks[index].name
-    const isCompleted = tasks[index].completed;
-    if (position === Number(index)) tasksNew.push({name: name, completed: !isCompleted});
-    if (position !== Number(index)) tasksNew.push({name: name, completed: isCompleted});
+
+  for (const i in tasks) {
+    const name = tasks[i].name;
+    const status = tasks[i].completed;
+    if (i === position) tasksNew.push({ name: name, completed: !status });
+    if (i !== position) tasksNew.push({ name: name, completed: status });
   }
+
   tasks = tasksNew;
+  renderTasks(tasks);
 };
 
-addTask("teste0", false);
-addTask("teste1", false);
-addTask("teste2", false);
-addTask("teste3", true);
-toggleStatusTask(0);
-toggleStatusTask(3);
-// removeTask(0);
-// removeTask(1);
+const renderTasks = (tasks) => {
+  $listTasks.innerHTML = '';
 
-console.log(tasks);
+  for (const index in tasks) {
+    const $itemOfListTasks = document.createElement('li');
+
+    $itemOfListTasks.addEventListener('dblclick', () => {
+      removeTask(index);
+    });
+
+    $itemOfListTasks.addEventListener('click', () => {
+      toggleStatusTask(index);
+    });
+
+    if (tasks[index].completed) $itemOfListTasks.textContent = `Tarefa: ${tasks[index].name} - Tarefa finalizada`;
+    if (!tasks[index].completed) $itemOfListTasks.textContent = `Tarefa: ${tasks[index].name}`;
+
+    $listTasks.appendChild($itemOfListTasks);
+  }
+};
+
+$buttonAddTask.addEventListener('click', () => {
+  if (!$inputAddTask.value.trim()) return;
+  addTask($inputAddTask.value, $inputCheckBox.checked);
+  $inputAddTask.value = '';
+  $inputCheckBox.checked = false;
+});
+
+$inputAddTask.addEventListener('keypress', (event) => {
+  if (!$inputAddTask.value.trim()) return;
+  if (event.key === 'Enter') {
+    addTask($inputAddTask.value, $inputCheckBox.checked);
+    $inputAddTask.value = '';
+    $inputCheckBox.checked = false;
+  }
+});
